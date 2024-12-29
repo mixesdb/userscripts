9e03e92456c2d25cdf449b1ad61fd559f7f70770
Martin <subfader@gmail.com>
2024-12-29 15:01:38 +0100

    SC initial upload

---
+++ SoundCloud/script.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         SoundCloud (by MixesDB)
+// @author       User:Martin@MixesDB (Subfader@GitHub)
+// @version      2024.12.29.1
+// @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
+// @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
+// @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
+// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.user.jsscript.user.js
+// @downloadURL  https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.user.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-TrackId.net_53
+// @include      http*soundcloud.com*
+// @noframes
+// @run-at       document-end
+// ==/UserScript==
+
+alert( "Test" );
