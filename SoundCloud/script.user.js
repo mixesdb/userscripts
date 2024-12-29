00808bf02b9844da910dc1ba45a52e4b82750f31
Martin <subfader@gmail.com>
2024-12-29 19:56:40 +0100

    Faved player highlighted

--- SoundCloud/script.user.js
+++ SoundCloud/script.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         SoundCloud (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2024.12.29.5
+// @version      2024.12.29.6
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
 // @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.user.js
+// @downloadURL  https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.user.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-SoundCloud_1
@@ -44,14 +44,6 @@ loadRawCss( pathRaw + "SoundCloud/script.css?v-" + scriptName + "_" + cacheVersi
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- * Funcs to be moved to scipt.funcs.js
- *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-
-
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *
  * Artwork
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -69,4 +61,28 @@ function artwork_wait(jNode) {
     if( typeof artwork_url  !== "undefined" ) {
         append_artwork( artwork_url );
     }
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Stream players > Favorite button
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+waitForKeyElements(".soundList__item .sc-button-like:not(.mdb-processed-favorited)", onFavoriteButton_wait);
+function onFavoriteButton_wait(jNode) {
+    logFunc( "onFavoriteButton_wait" );
+
+    // is favorited
+    if( jNode.hasClass("sc-button-selected") ) {
+        var title = jNode.closest(".soundList__item").find(".soundTitle__title");
+        log( "Favorite found: " + title.text() );
+
+        // Highlight player title if favorited
+        title.addClass("mdb-darkorange");
+    }
+
+    // mark as processed
+    jNode.addClass("mdb-processed-favorited");
 }
