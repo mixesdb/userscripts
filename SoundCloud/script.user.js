8bbbbb07fd72dbe25209038a62a8263e4099cf60
Martin <subfader@gmail.com>
2025-01-06 14:53:01 +0100

    Update script.user.js
    playlist links

--- SoundCloud/script.user.js
+++ SoundCloud/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SoundCloud (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.01.02.2
+// @version      2025.01.06.1
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -10,7 +10,7 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-SoundCloud_1
-// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.funcs.js?v_1
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/SoundCloud/script.funcs.js?v_2
 // @include      http*soundcloud.com*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=soundcloud.com
 // @noframes
@@ -26,7 +26,7 @@
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 var dev = 0,
-    cacheVersion = 1,
+    cacheVersion = 2,
     scriptName = "SoundCloud",
     repo = ( dev == 1 ) ? "Subfader" : "mixesdb",
     pathRaw = "https://raw.githubusercontent.com/" + repo + "/userscripts/refs/heads/main/";
@@ -42,7 +42,6 @@ loadRawCss( pathRaw + scriptName + "/script.css?v-" + cacheVersion );
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 waitForKeyElements(".listenArtworkWrapper", function( jNode ) {
-    logFunc( "artwork_wait" );
     log( location.href );
 
     // Artwork link tzo original
@@ -66,8 +65,6 @@ waitForKeyElements(".listenArtworkWrapper", function( jNode ) {
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 waitForKeyElements(".soundList__item .sc-button-like:not(.mdb-processed-favorited)", function( jNode ) {
-    logFunc( "onFavoriteButton_wait" );
-
     // is favorited
     if( jNode.hasClass("sc-button-selected") ) {
         var title = jNode.closest(".soundList__item").find(".soundTitle__title");
@@ -79,4 +76,42 @@ waitForKeyElements(".soundList__item .sc-button-like:not(.mdb-processed-favorite
 
     // mark as processed
     jNode.addClass("mdb-processed-favorited");
+});
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Playlist links
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// player links and link buttons
+// https://soundcloud.com/resident-advisor/sets/ra-podcast
+waitForKeyElements(".listenDetails__trackList li.trackList__item a.trackItem__trackTitle", function( jNode ) {
+    var playerUrlFixed = linkRemoveSetParameter( jNode.attr("href") );    
+    
+    jNode.attr( "href", playerUrlFixed )
+         .attr( "target", "_blank" );
+});
+
+// Compact playlists
+// https://soundcloud.com/resident-advisor
+waitForKeyElements(".compactTrackList__listWrapper li.compactTrackList__item span.compactTrackListItem__trackTitle", function( jNode ) {
+    var playerUrlFixed = linkRemoveSetParameter( jNode.attr( "data-permalink-path") );
+    
+    jNode.after( '<a href="'+playerUrlFixed+'" target="_blank" class="mdb-element mdb-copyLink sc-link-dark sc-link-primary sc-font-light">Link</a>' );
+});
+
+// .copyLink on click open new tab
+waitForKeyElements(".mdb-copyLink", function( jNode ) {
+    jNode.click(function(){
+        var url = $(this).attr("href");
+        window.open( url, "_blank" );
+    });
+});
+
+// button to copy link (no href)
+// hide it (would copy url with in parameter)
+waitForKeyElements(".listenDetails__trackList li.trackList__item button.sc-button-copylink", function( jNode ) {
+    jNode.hide();
 });
