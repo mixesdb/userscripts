b3d68db3f377557df51054af6fbade4ff894f0a4
Martin <subfader@gmail.com>
2025-01-11 17:13:28 +0100

    BBC and 1001 added

---
+++ 1001_Tracklists/script.user.js
@@ -0,0 +1,130 @@
+// ==UserScript==
+// @name         1001 Tracklists (by MixesDB)
+// @author       User:Martin@MixesDB (Subfader@GitHub)
+// @version      2025.01.11.1
+// @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
+// @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
+// @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
+// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/1001_Tracklists/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/1001_Tracklists/script.user.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-1001_Tracklists_1
+// @include      http*1001tracklists.com*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=1001tracklists.com
+// @noframes
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Load @ressource files with variables
+ * global.js URL needs to be changed manually
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var dev = 0,
+    cacheVersion = 1,
+    scriptName = "1001_Tracklists",
+    repo = ( dev == 1 ) ? "Subfader" : "mixesdb",
+    pathRaw = "https://raw.githubusercontent.com/" + repo + "/userscripts/refs/heads/main/";
+
+loadRawCss( pathRaw + "includes/global.css?v-" + scriptName + "_" + cacheVersion );
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * main
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function thousandoneTl() {
+    run1001 = false;
+
+    $(".adRow").remove();
+	// remove hidden elements that appear in text
+	$(".tlUserInfo").remove();
+	$(".tgHid").remove();
+
+    var t = $("#tlTab");
+    if(t.length > 0 ) {
+        var tl = "",
+            li = $("#tlTab > div"),
+            len = li.length,
+            rows = len;
+
+        li.each(function() {
+
+            // track
+            if( $(this).attr("data-trno") != "" ) {
+                var track = "",
+                    song = $("div .trackValue",this).text().trim(),
+                    label = $("div[itemprop='tracks'] .trackLabel",this).text().trim().toLowerCase(),
+                    dur = $("div[data-mode='hours']",this).text().trim();
+
+                if( dur != "" ) track = "["+dur+"] ";
+                track += song;
+                if( label != "" ) track += " ["+label+"]";
+
+                //xc( track );
+                if( track != "" && track != " " )  {
+                    tl += track + "\n";
+                }
+            }
+
+            // chapter
+            // https://www.1001tracklists.com/tracklist/y3gt5lt/dominik-koislmeyer-jetique-energy-extravadance-charts-2021-08-28.html
+            if( $(".fRow",this).length === 1 ) {
+                var chapter = $(".fRow a",this).text().trim();
+                if( chapter != "" ) tl += ";" + chapter + "\n";
+            }
+
+            // song chapter
+            var span = $("span",this);
+            if( span.attr("id") && span.attr("id").replace(/.+_(headtext_column)$/g, "$1") == "headtext_column" ) {
+                var intro = span.text().trim().replace(/:$/g,"");
+                if( intro != "" ) tl += "''" + intro + ":'' ";
+            }
+        });
+
+        xc( tl );
+
+        // fixes
+		var tl = tl.replace('&thinsp;', ' ')
+                   .replace(' (ID Remix) (ID Remix)', ' (ID Remix)')
+                   .replace(/;(.+)\n\n;(.+)/g, ';$1 - $2')
+                   .replace(/undefined - undefined/gi, '?');
+
+        // dur fixes
+		if( /\[\d+:\d+]/.test(tl) ) {
+			tl = tl.replace( /\[(\d)] /, "[0$1:00] " )
+			       .replace( /\[(\d+)] /, "[$1:00] " );
+		}
+		if( /\[\d\d:\d\d]/.test(tl) && /\[1:\d\d:\d\d]/.test(tl) ) {
+			tl = tl.replace( /\[(\d\d:\d\d)] /gm, "[0:$1] " );
+			tl = tl.replace( /\[(\d:\d\d)] /gm, "[0:0$1] " );
+		}
+
+		var res = apiTracklist( tl, "thousandoneTl" ),
+			tlApi = res.text,
+			feedback = res.feedback;
+
+		if( tlApi ) {
+			t.prepend( ta );
+            $("#mixesdb-TLbox").css("position","inherit").append( tlApi );
+		    fixTLbox( res.feedback );
+		}
+    }
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Run funcs
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+if( urlPath(1) == "tracklist") {
+    waitForKeyElements("#tlTab .trackValue", function( jNode ) {
+        if(run1001) thousandoneTl();
+    });
+}
---
+++ BBC/script.user.js
@@ -0,0 +1,135 @@
+// ==UserScript==
+// @name         BBC (by MixesDB)
+// @author       User:Martin@MixesDB (Subfader@GitHub)
+// @version      2025.01.11.1
+// @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
+// @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
+// @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
+// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/BBC/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/BBC/script.user.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
+// @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-BBC_1
+// @include      http*bbc.co.uk*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bbc.co.uk
+// @noframes
+// @grant        unsafeWindow
+// @run-at       document-end
+// ==/UserScript==
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Load @ressource files with variables
+ * global.js URL needs to be changed manually
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+var dev = 0,
+    cacheVersion = 1,
+    scriptName = "BBC",
+    repo = ( dev == 1 ) ? "Subfader" : "mixesdb",
+    pathRaw = "https://raw.githubusercontent.com/" + repo + "/userscripts/refs/heads/main/";
+
+loadRawCss( pathRaw + "includes/global.css?v-" + scriptName + "_" + cacheVersion );
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * main
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+function mainFX() {
+    var ep = $(".prog-layout.programmes-page"),
+        ul = $("ul.segments-list__items"),
+        rows = $("li", ul).length,
+    tl = "";
+
+    if(rows > 0 ) {
+    if( $("#tlEditor").length === 0 ) $(ep).prepend('<div id="tlEditor" class="fs75p"></div>' );
+
+      $("> li, > li ul li", ul).each(function(){
+
+          /* Chapters */
+          if( $(this).hasClass('segments-list__item--group') ) {
+              var chapter = $("> h3", this).text().trim();
+              //log("C: " + chapter);
+              tl += "\n;" + chapter + "\n";
+          }
+
+          // Track items */
+          if( $(this).hasClass('segments-list__item--music') ) {
+              /* Build artist */
+              //var artist = $('.segment__track span.artist', this).text();
+              //artist can be 2 span.artist joined by " and " > bbc.co.uk/programmes/b03dshct
+              var artist = "Unknown",
+                  artistItem = $('.segment__track h3 span.artist, .segment__track h4 span.artist', this),
+                  artistCount = artistItem.length;
+              if( artistCount == 1 ) {
+                  var artist = artistItem.text().trim();
+              }
+              if( artistCount > 1 ) {
+                  var artist = "";
+                  artistItem.each(function(){
+                      artist += $(this).text() + " & ";
+                  });
+                  var artist = artist.trim().replace(/ &$/g, "");
+              }
+              var artist = artist.replace(/\[Unknown]/gi, "Unknown");
+
+              /* Build title */
+              //fix span.title "Artist - Title" like > bbc.co.uk/programmes/b03f4lgk
+              //var titleRx = new RegExp(artist + " - ", "g");
+              //var title = title.replace(titleRx, '');
+              var title = $('.segment__track p.no-margin', this).text().replace(/\s/gm," ").trim();
+
+
+              /* Build label */
+              var label = $('.segment__track abbr[property="publisher"] span', this).text()
+                     .trim().replace(/^Unknown$/gi, "");
+
+              log("A: " + artist);
+              log("T: " + title);
+              log("L: " + label);
+              log("---------------------------------------------------------------");
+
+              /* Build tracklist */
+              if(artist && title)  tl += "# " + artist + " - " + title;
+              if(label) tl += " ["+ label +"]";
+              if(artist && title)  tl += "\n";
+
+              /* End of Chapter? */
+              // Check if current li is last in nested ul group
+              if( $(this).is(':last-child') ) {
+                  if( $(this).parent("ul").hasClass('segments-list__group-items') ) {
+                      tl += "\n;Chapter\n";
+                  }
+              }
+          }
+      });
+    }
+
+  // API
+  if( $("#mixesdb-TLbox").length === 0 ) {
+        log( tl );
+        var res = apiTracklist( tl, "standard" ),
+      tlApi = res.text,
+            tlApiFix = tlApi.replace( /\[/g, "(" ).replace( /\]/g, ")" ),
+      feedback = res.feedback;
+
+    if( tlApiFix ) {
+      $("#tlEditor").append('<textarea id="mixesdb-TLbox" class="mono" style="width:100%; margin:10px 0 0 0;">'+tlApiFix+'</textarea>');
+      fixTLbox( res.feedback );
+    }
+  }
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Run funcs
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+waitForKeyElements("ul.segments-list__items", mainWait);
+function mainWait(jNode) {
+    mainFX();
+}
