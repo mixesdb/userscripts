847e9f856f6caa7835fb72ac19d760ffa514595a
Martin <subfader@gmail.com>
2025-01-14 11:15:28 +0100

    New API features related funcs

--- SoundCloud/script.funcs.js
+++ SoundCloud/script.funcs.js
@@ -55,3 +55,120 @@ function linkRemoveSetParameter( url ) {
     return url.replace( /^(.+)\?in=.+$/, "$1" )
               .replace( /^(.+)\?in_system_playlist=.+$/, "$1" );
 }
+
+
+/*
+ * getScAccessTokenFromApi
+ * Get access_token and set as cookie
+ * SC's user cookie 'oauth_token' doesn't work with the API
+ */
+const apiUrlToolsX = "https://www.mixesdb.com/tools/api/api.php",
+      scAccessToken_cookie_id = "SoundCloud_userscript_by_MixesDB_scAccessToken",
+      scAccessToken_cookie_expire = 0.0063; // 0.0063 = 3599 secs in days
+
+function getScAccessTokenFromApi(handleData) {
+    logFunc( "getScAccessTokenFromApi" );
+
+    var scAccessToken_fromCookie = ( typeof( Cookies.get(scAccessToken_cookie_id) ) !== "undefined" ) ? Cookies.get( scAccessToken_cookie_id ) : '';
+
+    // check if set as cookie
+    if( scAccessToken_fromCookie == "" ) {
+        log( "token not in cookie" );
+
+        $.ajax({
+            type: "POST",
+            url: apiUrlToolsX,
+            data: { query: "getScAccessToken" }
+        })
+        .fail(function() {
+            console.log( "Cannot access MixesDB API or error!" );
+        })
+        .done(function(data) {
+            log( "API called. data: " + data );
+
+            var dataParsed = jQuery.parseJSON( data );
+            log( "data parsed: " + data.access_token );
+
+            if( dataParsed !== null ) {
+                handleData( dataParsed.access_token );
+                Cookies.set( scAccessToken_cookie_id, dataParsed.access_token, { expires: scAccessToken_cookie_expire, domain: domain } );
+            }
+        });
+    } else {
+        log( "token in cookie :)" );
+        handleData( scAccessToken_fromCookie );
+    }
+}
+
+
+/*
+ * append_fileDetails()
+ */
+function append_fileDetails( duration, soundActions, bytes="" ) {
+    log( "append_fileDetails(): " + duration );
+
+    var dur = convertHMS(  Math.floor(duration / 1000)  );
+
+    if( dur !== null ) {
+        soundActions.after('<button id="mdb-fileInfo" class="'+soundActionFakeButtonClass+' mdb-toggle" data-toggleid="mdb-fileDetails" title="Click to copy file details" class="pointer">'+dur+'</button>');
+
+        var fileDetails = '<div id="mdb-fileDetails" style="display:none"><textarea class="mdb-selectOnClick" rows="9">{|{{NormalTableFormat-Bytes}}\n! dur\n! bytes\n! kbps\n|-\n| '+dur+'\n| '+bytes+'\n| \n|}</textarea></div>';
+        $("#mdb-toggle-target").append( fileDetails );
+    }
+}
+
+
+/*
+ * formatScDate()
+ */
+function formatScDate( date ) {
+    if( typeof(date) !== "undefined" ) {
+        date = date.replace(/(\d\d\d\d)\/(\d\d)\/(\d\d).+$/g,"$1-$2-$3");
+    } else {
+        date = "";
+    }
+    return date;
+}
+
+
+/*
+ * fixScRedirectUrl()
+ */
+function fixScRedirectUrl( url ) {
+    // https://gate.sc/?url=http%3A%2F%2Fbit.ly%2FHenPod&token=df8575-1-1631362609871
+    url = decodeURIComponent( url.replace(/^.+url=(.+)&token.+$/, "$1") );
+    return url;
+}
+
+
+/*
+ * toggle click
+ */
+waitForKeyElements(".mdb-toggle", function( jNode ) {
+    jNode.click(function(){
+        var toggleId = $(this).attr("data-toggleid");
+        log( toggleId );
+
+        $("#"+toggleId).slideToggle();
+        $(this).toggleClass("selected");
+
+        if( toggleId == "mdb-fileDetails" ) $("#mdb-fileDetails textarea").click();
+    });
+});
+
+
+/*
+ * mdb-select-onClick
+ */
+waitForKeyElements(".mdb-selectOnClick", function( jNode ) {
+    jNode.click(function(){
+        log( "click" );
+        $(this).addClass("selected").select().focus();
+
+        var tagName = $(this).prop("tagName");
+        //log( tagName );
+        if( tagName == 'DATE' || tagName == "H1" ) {
+            selectText( $(this).attr("id") );
+        }
+    });
+});
