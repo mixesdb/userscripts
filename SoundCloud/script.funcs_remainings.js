95fd2d891c65278abc559b434be2939640b56a74
Martin <subfader@gmail.com>
2025-01-14 10:21:04 +0100

    Delete SoundCloud/script.funcs_remainings.js

--- SoundCloud/script.funcs_remainings.js
+++
@@ -1,6 +0,0 @@
-/*
- * selectText()
- */
-function selectText(e){
-    var t=document.getElementById(e);var n=window.getSelection();var r=document.createRange();r.selectNodeContents(t);n.removeAllRanges();n.addRange(r)
-}
