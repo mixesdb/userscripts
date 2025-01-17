d4c0f03f773567866308ba1f81384ffc5c3f0cfe
Martin <subfader@gmail.com>
2025-01-17 15:24:13 +0100

    SoundCloud: right sidebar fix

--- SoundCloud/script.user.js
+++ SoundCloud/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SoundCloud (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.01.17.7
+// @version      2025.01.17.8
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -26,8 +26,8 @@
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-var dev = 0,
-    cacheVersion = 8,
+var dev = 1,
+    cacheVersion = 9,
     scriptName = "SoundCloud",
     repo = ( dev == 1 ) ? "Subfader" : "mixesdb",
     pathRaw = "https://raw.githubusercontent.com/" + repo + "/userscripts/refs/heads/main/";
