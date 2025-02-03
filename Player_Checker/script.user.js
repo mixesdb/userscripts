c4bee3f5485d6e6751b2b7d5779d9bde53a5905d
Martin <subfader@gmail.com>
2025-02-03 13:06:40 +0100

    Update script.user.js

--- Player_Checker/script.user.js
+++ Player_Checker/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Player Checker (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.02.03.3
+// @version      2025.02.03.4
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -13,6 +13,7 @@
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-Player_Checker_5
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/SoundCloud/api_funcs.js?v-Player_Checker_1
 // @include      http*groove.de/*/*/*/*podcast*
+// @include      http*ra.co/news/*
 // @include      http*ra.co/podcast/*
 // @include      http*wearesoundspace.com/*
 // @include      http*toxicfamily.de/*/*/*/*
