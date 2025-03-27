fa8a6f11cd1668ecb29b1a2cf2a7700cb7635f5f
Martin <subfader@gmail.com>
2025-03-28 00:30:31 +0100

    Version fix

--- Apple_Music/script.user.js
+++ Apple_Music/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Apple Music (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.01.20.1
+// @version      2025.03.28.1
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -9,7 +9,7 @@
 // @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/Apple_Music/script.user.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-Apple_Music_3
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-Apple_Music_5
 // @match        https://*music.apple.com/*
 // @match        https://*beta.music.apple.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=music.apple.com
@@ -150,10 +150,10 @@ setTimeout(function() {
                     log( "No soup for you! *.music.apple.com doesn't allow external resources like api.php" );
 
                     var output = "",
-                        rowCount = tl_cuesAsDur.split("\n").length;
+                        rowCount = tl_cuesAsDur.split("\n").length - 1;
 
                     output += '<table id="mdb-tl-fakeOutput">';
-                    output += '<td id="mdb-noSoup-wrapper"><img src="'+noSoupForYou_base64Url+'" width="270" alt="No soup for you!"></td><td>';
+                    output += '<td id="mdb-noSoup-wrapper"><img src="'+noSoupForYouUrl+'" width="270" alt="No soup for you!"></td><td>';
                     output += '<p class="mdb-highlight">music.apple.com restricts loading external resources like the Tracklist Editor API.<br />Format this to the standard format by pasting into the Tracklist Editor manually.</p>';
                     output += '<textarea id="mixesdb-TLbox" class="mdb-tlBox mono mdb-selectOnClick" rows="'+rowCount+'">'+tl_cuesAsDur+'</textarea>';
 
