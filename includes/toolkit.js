b11b72cba0758ed58f36e2039c1847af2347b4eb
Martin <subfader@gmail.com>
2025-03-30 20:07:03 +0200

    Update toolkit.js

--- includes/toolkit.js
+++ includes/toolkit.js
@@ -260,8 +260,8 @@ function containsSpecialCharacters( text ) {
 function apiUrl_searchKeywords_fromUrl( thisUrl ) {
     var keywords = mixesdbPlayerUsage_keywords( thisUrl );
 
-    // Do not search with quotes, otherwise special characters in player URLs are not found
-    
+    // Quotes are needed to avoid false results
+    // but with quotes special characters in URLs are not found…    
     if( containsSpecialCharacters(thisUrl) ) {
         // https://www.mixesdb.com/w/api.php?action=query&list=search&srprop=timestamp&format=json&srsearch=insource:mixcloud.com/ElectronicBunker/sov-podcast-001-sub%CA%9Eutan
         return 'https://www.mixesdb.com/w/api.php?action=query&list=search&srprop=timestamp&format=json&srsearch=insource:'+keywords;
