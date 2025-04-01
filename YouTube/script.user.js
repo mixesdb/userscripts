72590b14b00f29cf38ce57f1cebfa9d06b531ab1
Martin <subfader@gmail.com>
2025-04-01 17:59:13 +0200

    Update script.user.js

--- YouTube/script.user.js
+++ YouTube/script.user.js
@@ -10,8 +10,8 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-YouTube_3
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit__.js?v-YouTube_4
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-YouTube_5
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-YouTube_5
 // @include      http*youtube.com*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @noframes
