30e6e117bb9d8d50f949ee90654516f5016941c5
Martin <subfader@gmail.com>
2025-04-06 20:52:12 +0200

    TID: More elegant allowUserTableMarking

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.04.06.6
+// @version      2025.04.06.7
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -11,7 +11,7 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-TrackId.net_93
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-TrackId.net_61
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-TrackId.net_62
 // @include      http*trackid.net*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=trackid.net
 // @noframes
@@ -136,7 +136,8 @@ function checkTidIntegration( tidPlayerUrl="", mdbPageId="", action="", wrapper=
                                 if( target == "table" ) {
                                     waiter.remove();
 
-                                    var notYetIntegratedText = '<span class="tooltip-title small" title="This tracklist is not intergated yet to the found mix page">not yet</span>';
+                                    var allowUserTableMarking = ["Schrute_Inc._disabled", "Komapatient"].includes(currentUsername),
+                                        notYetIntegratedText = '<span class="tooltip-title small" title="This tracklist is not intergated yet to the found mix page">not yet</span>';
 
                                     if( checked_pageId ) {
                                         var lastCheckedAgainstMixesDB = data.mixesdbtrackid[0].mixesdbpages[0].lastCheckedAgainstMixesDB;
@@ -152,7 +153,7 @@ function checkTidIntegration( tidPlayerUrl="", mdbPageId="", action="", wrapper=
                                             // Add checkbox in tables for certain users
                                             var currentUsername = $(".user-name").text();
 
-                                            if( currentUsername == "Schrute_Inc._disabled" || currentUsername == "Komapatient" ) {
+                                            if( allowUserTableMarking ) {
                                                 var status_td = wrapper.prev("td.status"),
                                                     status = $("div.MuiBox-root",status_td).attr("aria-label").trim();
 
@@ -185,7 +186,7 @@ function checkTidIntegration( tidPlayerUrl="", mdbPageId="", action="", wrapper=
                                                         mdbPageId = resultsArr[0].pageid,
                                                         currentUsername = $(".user-name").text();
 
-                                                    if( mdbPageId && currentUsername == "Schrute_Inc." || currentUsername == "Komapatient" ) {
+                                                    if( mdbPageId && allowUserTableMarking ) {
                                                         var status_td = wrapper.prev("td.status"),
                                                             status = $("div.MuiBox-root",status_td).attr("aria-label").trim();
 
