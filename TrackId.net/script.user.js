651024d0f045190d210c1005ae7f0adab877b8ec
Martin <subfader@gmail.com>
2025-06-08 20:03:37 +0200

    TID + major labels

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.06.08.8
+// @version      2025.06.08.9
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -81,7 +81,7 @@ String.prototype.removeMajorLabels = function() {
                    .replace( /(^|, )EMI( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )Epic( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )High Fashion Music( [^\]]+)?$/gi, '' )
-                   .replace( /(^|, )Island Records( [^\]]+)?$/gi, '' )
+                   .replace( /(^|, )Island (Mercury|Records)( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )Metrophon( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )PLG UK( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )Polydor( [^\]]+)?$/gi, '' )
