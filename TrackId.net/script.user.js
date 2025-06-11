038c96a538b4347a06b0d05a65881e8236922c49
Martin <subfader@gmail.com>
2025-06-11 11:41:52 +0200

    TID: major label additions

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.06.08.11
+// @version      2025.06.11.1
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -95,6 +95,7 @@ String.prototype.removeMajorLabels = function() {
                    .replace( /(?:^|, )WM Germany(?: - )([^\]]+)?$/gi, '$1' )
                    // different Catalogs
                    .replace( /(^|, )(Clarence Avant|Onelove|PIAS) (Recordings )?Catalog(ue)?( [^\]]+)?$/gi, '' )
+                   .replace( /(^|, )Recordings Catalogue( [^\]]+)?$/gi, '' ) // yes, "Recordings Catalogue"! https://trackid.net/audiostreams/subfader-the-ghetto-funk-show-summer-beats-20090119
                    ;
     return text;
 };
