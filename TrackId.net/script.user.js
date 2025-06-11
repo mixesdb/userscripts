cecff8981aa856528f85363e4ea214e752a4bd0e
Martin <subfader@gmail.com>
2025-06-11 12:20:35 +0200

    Update script.user.js

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.06.11.2
+// @version      2025.06.11.3
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -91,6 +91,7 @@ String.prototype.removeMajorLabels = function() {
                    .replace( /(^|, )(UMC|Universal Music)( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )Ultra, LLC( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )UNI\/MOTOWN( [^\]]+)?$/gi, '' )
+                   .replace( /(^|, )Verve Reissues( [^\]]+)?$/gi, '' )
                    .replace( /(^|, )Warner( [^\]]+)?$/gi, '' )
                    .replace( /(?:^|, )WM Germany(?: - )([^\]]+)?$/gi, '$1' )
                    // different Catalogs
