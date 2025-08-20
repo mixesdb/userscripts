a4e6ea652511ee68954c103207d30ae6f10f6402
Martin <subfader@gmail.com>
2025-08-20 14:37:55 +0200

    Tracklist merger improved for artist1 & artist2 swicthes

--- Tracklist_Merger/script.user.js
+++ Tracklist_Merger/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tracklist Merger (Beta)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.08.20.3
+// @version      2025.08.20.4
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -283,7 +283,6 @@ function mergeTracklists(original_arr, candidate_arr) {
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
  * Diff functions
- * ChatGPT 4o-mini-high v2
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
