27bee936fb0fa76020bbc576da0fafa16d247e5c
Martin <subfader@gmail.com>
2025-08-27 14:25:42 +0200

    Update script.user.js

--- YouTube/script.user.js
+++ YouTube/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2025.08.27.9
+// @version      2025.08.27.10
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -10,7 +10,7 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-YouTube_12
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-YouTube_13
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-YouTube_12
 // @include      http*youtube.com*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
@@ -80,7 +80,7 @@ function getDurationSec_YT() {
 if( ytId ) {
     var playerUrl = "https://youtu.be/" + ytId,
         dur_sec_cache = null;
-  
+
     waitForKeyElements( "#bottom-row", function( jNode ) {
         var titleText = $("#title h1").text(),
             wrapper = jNode;
@@ -111,20 +111,3 @@ if( ytId ) {
         }, 1000 );
     }, true );
 }
-
-waitForKeyElements( ".mdb-toggle", function( jNode ) {
-    jNode.on("click", function() {
-        var toggleId = $(this).attr("data-toggleid"),
-            target = $("#"+toggleId);
-
-        target.slideToggle( msWaitToggle );
-        $(this).toggleClass("selected");
-
-        setTimeout(function() {
-            var textarea = target.find("textarea");
-            if( textarea.length && textarea.is(":visible") ) {
-                textarea.trigger("focus").select();
-            }
-        }, msWaitToggle);
-    });
-});
