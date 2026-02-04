4c9790097caa9ccc7a8d6970b81143b576d41fba
Martin <subfader@gmail.com>
2026-02-04 15:44:27 +0100

    Fix NTS tracklist case matching

--- NTS/script.user.js
+++ NTS/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NTS (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.02.04.1
+// @version      2026.02.04.2
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -145,11 +145,15 @@ waitForKeyElements("ul.tracklist__tracks .track__timestamp", function(jNode){
 // Case 2: timestamps are NOT available (promo element exists)
 waitForKeyElements("ul.tracklist__tracks div.tracklist-promo:not(.hidden)", function(jNode){
     var tlE = jNode.closest("ul.tracklist__tracks:not(.processed)");
-    ntsProcessTracklists(tlE);
+    if(!tlE.find(".track__timestamp").length){
+        ntsProcessTracklists(tlE);
+    }
 });
 
 // Case 3: No timestamps at all
 waitForKeyElements("ul.tracklist__tracks:not(.processed)", function(jNode){
-    ntsProcessTracklists(jNode);
-    jNode.addClass("processed");
+    if(!jNode.find(".track__timestamp").length && !jNode.find("div.tracklist-promo:not(.hidden)").length){
+        ntsProcessTracklists(jNode);
+        jNode.addClass("processed");
+    }
 });
