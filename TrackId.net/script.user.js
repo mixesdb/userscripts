0f336b8d9309ef24d725c32d3f96d1d5a5eba2d2
Martin <subfader@gmail.com>
2026-02-11 12:46:38 +0100

    Add cue format switch button and extract cue conversion helpers

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.02.04.5
+// @version      2026.02.11.1
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -12,6 +12,7 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-TrackId.net_109
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-TrackId.net_77
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/Tracklist_Cue_Switcher/script.funcs.js?v_1
 // @include      http*trackid.net*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=trackid.net
 // @noframes
@@ -812,6 +813,39 @@ waitForKeyElements(".mdb-tid-table:not('.tlEditor-processed')", function( jNode 
     }
 });
 
+
+
+function toggleTracklistTextareaCueFormat() {
+    var ta = $("textarea.mixesdb-TLbox");
+    if (!ta.length) return;
+
+    var currentFormat = ta.attr("data-mdb-cue-format") || "MM";
+    var nextFormat = currentFormat === "MM" ? "HMM" : "MM";
+
+    var lines = String(ta.val() || "").split("\n");
+    var convertedLines = lines.map(function (line) {
+        return line.replace(/^\s*\[\s*([0-9\?:]+)\s*\]/, function (m, cue) {
+            return "[" + toggleCue_MM_HMM(cue) + "]";
+        });
+    });
+
+    ta.val(convertedLines.join("\n"));
+    ta.attr("data-mdb-cue-format", nextFormat);
+
+    var buttonLabel = nextFormat === "HMM" ? "Switch cue format (h:m > mmm)" : "Switch cue format (mmm > h:m)";
+    $("#switchCueFormat").text(buttonLabel);
+}
+
+waitForKeyElements("ul#tlEditor-feedback-topInfo", function(jNode) {
+    if (!$("#switchCueFormat").length) {
+        jNode.prepend('<li class="info_switchCueFormat"><button id="switchCueFormat" class="hand">Switch cue format (mmm > h:m)</button></li>');
+    }
+});
+
+$(document).on("click", "#switchCueFormat", function(e) {
+    e.preventDefault();
+    toggleTracklistTextareaCueFormat();
+});
 
 // toggleTlCandidate
 waitForKeyElements("#toggleTlCandidate", function( jNode ) {
