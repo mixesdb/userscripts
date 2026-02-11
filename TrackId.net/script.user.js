f55efccc75975ad60214f0ae8110c67a2939ae25
Martin <subfader@gmail.com>
2026-02-11 17:25:48 +0100

    TrackId: click TL category helper buttons on MixesDB edit

--- TrackId.net/script.user.js
+++ TrackId.net/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TrackId.net (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.02.11.6
+// @version      2026.02.11.7
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -957,16 +957,14 @@ d.ready(function () {
         && wgTitle != "Main Page"
         && /^(incomplete|complete)$/.test(tidHasTl)
     ) {
-        var textarea = $("textarea#wpTextbox1");
+        waitForKeyElements("#afterTextbox1 a.button-after", function() {
+            var buttonSelector = tidHasTl == "incomplete" ? "a#button-after-TLi" : "a#button-after-TLc",
+                targetButton = $(buttonSelector);
 
-        if (textarea.length) {
-            textarea.val(
-                textarea.val().replace(
-                    "[[Category:Tracklist: none]]",
-                    "[[Category:Tracklist: " + tidHasTl + "]]"
-                )
-            );
-        }
+            if (targetButton.length) {
+                targetButton.trigger("click");
+            }
+        });
     }
 });
 
