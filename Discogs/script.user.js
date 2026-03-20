98d3157d0fcd53be637b5244971a8746f0b7d604
Martin <subfader@gmail.com>
2026-03-20 16:11:45 +0100

    Update script.user.js

--- Discogs/script.user.js
+++ Discogs/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Discogs (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.03.20.3
+// @version      2026.03.20.4
 // @description  Change the look and behaviour of the MixesDB website to enable feature usable by other MixesDB userscripts.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1293952534268084234
@@ -385,7 +385,10 @@ function getReleaseLabelFromMainSection(){
 			})
 			.filter(Boolean);
 
-		return norm(labelParts.join(" "));
+		return norm( labelParts
+                         .join(" ")
+                         .replace( "–", "-" )
+                   );
 	}
 
 	return "";
