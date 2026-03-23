28c8a37862e0902a5101416ccf7ead9f3de8975c
Martin <subfader@gmail.com>
2026-03-23 11:33:02 +0100

    Fix hearthis toolkit usage links

--- hearthis.at/script.user.js
+++ hearthis.at/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         hearthis.at (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.03.02.2
+// @version      2026.03.23.1
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -11,7 +11,7 @@
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/youtube_funcs.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-hearthis.at_8
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-hearthis.at_18
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/toolkit.js?v-hearthis.at_19
 // @include      http*hearthis.at*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=hearthis.at
 // @noframes
@@ -64,6 +64,6 @@ if( urlPath(2) && toolkitTarget.length == 1 && pageUrl != "" && pageId != "" ) {
 
     var pageUrl_short = 'https://hearthis.at/'+pageId+'/';
 
-    // Handles both URL variants
-    getToolkit( pageUrl_short, "playerUrl", "detail page", toolkitTarget, "after", titleText, "", 2, "" );
+    // Pass the canonical detail URL so toolkit.js can search both detail and short/embed variants
+    getToolkit( pageUrl, "playerUrl", "detail page", toolkitTarget, "after", titleText, "", 2, pageUrl_short );
 }
