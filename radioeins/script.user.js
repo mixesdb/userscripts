789ea4d422fad6e07ee8ea4b0a5db42b1b8e0285
Martin <subfader@gmail.com>
2026-04-27 18:11:25 +0200

    radioeins name fix

--- radioeins/script.user.js
+++ radioeins/script.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         radioeins.de (by MixesDB)
+// @name         radioeins (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.04.27.1
+// @version      2026.04.27.2
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
-// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/radioeins.de/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/radioeins.de/script.user.js
+// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/radioeinse/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/radioeinse/script.user.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
-// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-radioeins.de_1
+// @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-radioeins_1
 // @include      http*radioeins.de*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=radioeins.de
 // @noframes
@@ -17,7 +17,7 @@
 // ==/UserScript==
 
 var cacheVersion = 1,
-    scriptName = "radioeins.de";
+    scriptName = "radioeins";
 
 loadRawCss( githubPath_raw + "includes/global.css?v-" + scriptName + "_" + cacheVersion );
 
@@ -61,8 +61,10 @@ function buildTracklistFromTable( tableNode ) {
         return;
     }
 
-    var tlRaw = lines.join("\n"),
-        res = apiTracklist( tlRaw, "standard" ),
+    var tlRaw = lines.join("\n");
+    log( "tl before API:\n" + tlRaw );
+
+    var res = apiTracklist( tlRaw, "standard" ),
         tlApi = (res && res.text) ? res.text : tlRaw,
         feedback = (res && res.feedback) ? res.feedback : "",
         tlEditor = $('<div class="tlEditor mdb-radioeins-tracklist"></div>'),
