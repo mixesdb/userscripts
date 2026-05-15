0d049cd4671d4052123c311da20e16888649768d
Martin <subfader@gmail.com>
2026-05-15 17:10:26 +0200

    TheLotRadio name

--- TheLotRadio/script.user.js
+++ TheLotRadio/script.user.js
@@ -5,8 +5,8 @@
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
-// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/The_Lot_Radio/script.user.js
-// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/The_Lot_Radio/script.user.js
+// @updateURL    https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/TheLotRadio/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/TheLotRadio/script.user.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/jquery-3.7.1.min.js
 // @require      https://cdn.rawgit.com/mixesdb/userscripts/refs/heads/main/includes/waitForKeyElements.js
 // @require      https://raw.githubusercontent.com/mixesdb/userscripts/refs/heads/main/includes/global.js?v-The_Lot_Radio_3
