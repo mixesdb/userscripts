e0886ca86931f0500426c4b732d070b4c596d096
Martin <subfader@gmail.com>
2026-07-06 15:34:48 +0200

    Update script.user.js

--- Episodes_Importer/Hernan_Cattaneo_Resident/script.user.js
+++ Episodes_Importer/Hernan_Cattaneo_Resident/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hernan Cattaneo Resident (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.07.06.08
+// @version      2026.07.06.9
 // @description  Add MixesDB creation links to Hernan Cattaneo Resident podcast episodes.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -366,7 +366,7 @@ loadRawCss( githubPath_raw + "includes/global.css?v-" + scriptName + "_" + cache
             }
             .${config.classNames.link} {
                 display: inline-block;
-                margin: 0.35em 0 0.75em;
+                margin: -0.5em 0 0.75em;
                 padding: 0.25em 0.6em;
                 border-radius: 4px;
                 font-size: 0.85rem;
