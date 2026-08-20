05964514f4381bf1d8f5ee9537354796300b4639
Martin <subfader@gmail.com>
2026-08-20 19:07:28 +0200

    Toolkit: mdb-tooltip placement fix

--- 1001_Tracklists/script.user.js
+++ 1001_Tracklists/script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         1001 Tracklists (by MixesDB)
 // @author       User:Martin@MixesDB (Subfader@GitHub)
-// @version      2026.08.20.2
+// @version      2026.08.20.3
 // @description  Change the look and behaviour of certain DJ culture related websites to help contributing to MixesDB, e.g. add copy-paste ready tracklists in wiki syntax.
 // @homepageURL  https://www.mixesdb.com/w/Help:MixesDB_userscripts
 // @supportURL   https://discord.com/channels/1258107262833262603/1261652394799005858
@@ -30,7 +30,7 @@
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-var cacheVersion = 24,
+var cacheVersion = 25,
     scriptName = "1001_Tracklists";
 window.scriptName = scriptName; // toolkit.js reads this global directly
 
